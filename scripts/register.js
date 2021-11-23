a36edc4b3871851f579dcb4ea2f1ab001afc2425
marlonfrade <marlonmelo98@gmail.com>
2021-11-23 00:23:00 -0300

    tela login

--- scripts/register.js
+++ scripts/register.js
@@ -5,6 +5,19 @@ const eye2 = document.querySelector('.eye_button2');
 const svg_view = document.querySelector('.svg_view');
 const svg_hidden = document.querySelector('.svg_hidden');
 
+//Get Elements to make a function on change on input
+
+const email_input = document.querySelector('[email_input]');
+const password_input = document.querySelector('[password_input]');
+
+// function to change the opacity of the button on type;
+email_input.addEventListener('keyup', () => {
+    const button = document.getElementById('submitButton');
+    password_input.addEventListener('keyup', () => {
+        button.style.opacity = '1';
+
+    })
+})
 
 // function to show and hide the Password Value input
 eye2.addEventListener('click', () => {
